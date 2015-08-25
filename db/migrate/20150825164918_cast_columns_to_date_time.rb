9c01f0c253c120e8d32ce32e91fb19a9695793f3
Robert Strzępka <robert.strzepka@appunite.com>
2015-08-25 18:51:24 +0200

    Migration with cast column types

--- db/migrate/20150825164918_cast_columns_to_date_time.rb
+++ db/migrate/20150825164918_cast_columns_to_date_time.rb
@@ -1,5 +1,5 @@
-class ChangeReservationColumnTypes < ActiveRecord::Migration
-  def change
+class CastColumnsToDateTime < ActiveRecord::Migration
+      def change
 	change_column :reservations, :time_to, 'datetime USING CAST(time_to AS datetime)'
 	change_column :reservations, :time_from, 'datetime USING CAST(time_from AS datetime)'
   end
